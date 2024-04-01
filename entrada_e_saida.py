70e3b515c7b7dd1a3c5ba8e68dc9d47991389f12
Aline <alinealmeidadev@gmail.com>
2024-04-01 20:39:14 -0300

    add funções

---
+++ entrada_e_saida.py
@@ -0,0 +1,14 @@
+def saudar():
+    nome = input("Qual é o seu nome:")
+    print("Olá, ", nome, "! Seja bem vinda(o)!")
+
+saudar()
+
+def somar():
+    num_1 = int(input("Digite um número: "))
+    num_2 = int(input("Digite outro número: "))
+    soma = num_1 + num_2
+
+    print("A soma dos números é: ", soma)
+
+somar()
